30a490a35eb67b606221b28898e025a91f761549
Vince Punzalan <vpunzalan015@gmail.com>
2025-09-12 15:04:06 +0800

    adding all activities and challenges

---
+++ code challenge 3/codechallenge3.py
@@ -0,0 +1,11 @@
+username = "vince123"
+password = "test123"
+
+urname = input("Please put your username >>>>  ")
+psword = input("put your password >>>>  ")
+
+if username == urname:
+	print("access granted")
+
+else:
+	print("access denied")
