961f597ffee0b28b6434792c5d25f0b3669071ac
vince <vpunzalan015@gmail.com>
2025-09-17 11:44:43 +0800

    code challenge 8

---
+++ code_challenge8.py
@@ -0,0 +1,10 @@
+print("MULTIPLICATION TABLE MAKER")
+
+no = int(input("place one number here >>>>   "))
+
+
+print("Multiplication table for", no)
+
+for l in range(1, 11):
+
+    print(no, "x", l, "=", no * l)
