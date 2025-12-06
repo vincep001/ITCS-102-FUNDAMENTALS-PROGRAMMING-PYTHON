3871593382ba774376375da03672f3c7af903531
vince <vpunzalan015@gmail.com>
2025-12-06 15:42:35 +0800

    for loop
    triangle

---
+++ codechallenge13.py
@@ -0,0 +1,53 @@
+for i in range(1, 4, 1):
+    for z in range(1, 4, 1):
+        print(" ", end=" ")
+    for x in range(3, i, -1):
+        print(" ", end=" ")
+    for c in range(1, i, 1):
+        print("*", end=" ")
+    for v in range(0, i, 1):
+        print("*", end=" ")
+    print()
+for i in range(1, 4, 1):
+    for z in range(1, 4, 1):
+        print(" ", end=" ")
+    for x in range(0, i, 1):
+        print(" ", end=" ")
+    for c in range(2, i, -1):
+        print("*", end=" ")
+    for v in range(3, i, -1):
+        print("*", end=" ")
+    print()
+
+
+for i in range(1, 7, 1):
+    for z in range(6, i, -1):
+        print(" ", end=" ")
+    for x in range(1, i, 1):
+        print("*", end=" ") 
+    for c in range(0, i, 1):
+        print("*", end=" ")
+    print()
+for i in range(1, 7, 1):
+    for z in range(6, i, -1):
+        print(" ", end=" ")
+    for x in range(1, i, 1):
+        print("*", end=" ")
+    for c in range(0, i, 1):
+        print("*", end=" ")
+    print()
+for i in range(1, 7, 1):
+    for z in range(6, i, -1):
+        print(" ", end=" ")
+    for x in range(1, i, 1):
+        print("*", end=" ")
+    for c in range(0, i, 1):
+        print("*", end=" ")
+    print()
+
+for i in range(1, 4, 1):
+    for z in range(1, 9, 1):
+        print("", end=" ")
+    for x in range(1, 4, 1):
+        print("*", end=" ")
+    print()
